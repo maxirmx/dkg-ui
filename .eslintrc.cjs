21892f049f3ed4afb6f742abff4aec96a54fc3e0
Maxim [maxirmx] Samsonov <maxirmx@sw.consulting>
2024-04-16 00:50:32 +0300

    tests fix 3

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,9 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   ignorePatterns: ["doc/**"],
+  env: {
+    node: true
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
